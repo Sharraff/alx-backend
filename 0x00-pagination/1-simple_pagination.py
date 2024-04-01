6f6a730df10bbb1feb66e03def68b1745d8801e1
Sharraff <111119433+Sharraff@users.noreply.github.com>
2024-04-01 15:12:04 +0000

    changes

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -5,7 +5,6 @@ import math
 from typing import List, Tuple
 
 
-
 def index_range(page: int, page_size: int) -> Tuple[int, ...]:
     """Generate list index range based on pagination params"""
     start_index = page_size * (page - 1)
@@ -18,11 +17,9 @@ class Server:
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
-
     def __init__(self):
         self.__dataset = None
 
- 
     def dataset(self) -> List[List]:
         """Cached dataset
         """
@@ -34,7 +31,6 @@ class Server:
 
         return self.__dataset
 
-  
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Get current page dataset"""
         assert isinstance(page, int) and page > 0
